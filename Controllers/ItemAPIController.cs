9dad159f45197dfe603af8118867389ee91dffe8
ellyellyelly <18369514+ellyellyelly@users.noreply.github.com>
2024-11-27 22:15:54 -0800

    rudimentary get api set up for item listings. This will need to be updated to match routing standards later!

---
+++ Controllers/ItemAPIController.cs
@@ -0,0 +1,39 @@
+﻿using CyberMall.Data;
+using CyberMall.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient.DataClassification;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CyberMall.Controllers
+{
+    [Route("api/Item")]
+    [ApiController]
+    public class ItemAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemAPIController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public async Task<List<ItemListing>> Index(int? id)
+        {
+            if (id == null)
+            {
+                // To-Do: don't just leak everything and create a container object
+                return await _context.ItemListings.ToListAsync();
+            }    
+            else
+            {
+                List<ItemListing> itemList = new List<ItemListing>();
+                ItemListing listing = await _context.ItemListings.FindAsync(id);
+                itemList.Add(listing);
+                return itemList;
+            }
+        }
+
+    }
+}
