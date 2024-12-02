565a8a6b56be9bd4485a8f854e86aa70e3e52961
Eric Lighthall <55029902+Eric-Lighthall@users.noreply.github.com>
2024-12-02 03:58:30 -0800

    Add view and model for individual orders

---
+++ Models/ItemSale.cs
@@ -0,0 +1,10 @@
+﻿namespace CyberMall.Models
+{
+    public class ItemSale
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
